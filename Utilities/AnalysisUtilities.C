638ddee6d4f1bff3d90db7cb170c556f20be8ca4
Aimeric Landou <aimeric.landou@gmail.com>
2025-12-08 13:14:42 +0000

    AnalysisUtilities.C: update getNevents for gen

--- Utilities/AnalysisUtilities.C
+++ Utilities/AnalysisUtilities.C
@@ -54,11 +54,11 @@ double GetNEventsSelected_JetFramework_weighted(TFile* file_O2Analysis, const ch
 
 long int GetNEventsSelected_JetFramework_gen(TFile* file_O2Analysis, const char analysisWorkflow[]) {
   int ibinFinalSelection = mcCollHistIsObsolete ? 4 : 6;
-  return ((TH1I*)file_O2Analysis->Get((TString)analysisWorkflow+"/h_mcColl_counts"))->GetBinContent(ibinFinalSelection);
+  return ((TH1I*)file_O2Analysis->Get((TString)analysisWorkflow + mcCollHistIsObsolete ? "/h_mcColl_counts_weight" : "/h_mccollisions_weighted"))->GetBinContent(ibinFinalSelection);
 }
 double GetNEventsSelected_JetFramework_gen_weighted(TFile* file_O2Analysis, const char analysisWorkflow[]) {
   int ibinFinalSelection = mcCollHistIsObsolete ? 4 : 6;
-  return ((TH1F*)file_O2Analysis->Get((TString)analysisWorkflow+"/h_mcColl_counts_weight"))->GetBinContent(ibinFinalSelection);
+  return ((TH1F*)file_O2Analysis->Get((TString)analysisWorkflow + mcCollHistIsObsolete ? "/h_mcColl_counts_weight" : "/h_mccollisions_weighted"))->GetBinContent(ibinFinalSelection);
 }
 
 
